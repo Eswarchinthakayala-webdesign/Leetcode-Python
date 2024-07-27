33f5d5adf43d4c5bc8d93f2b1b011b6da725ecca
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2024-07-27 13:05:00 +0530

    Create code.py

---
+++ Easy Questions/20.Valid Parenthesis/code.py
@@ -0,0 +1,13 @@
+def isValid(s):
+    brackets={')':'(',']':'[','}':'{'}
+    stack=[]
+    for char in s:
+        if char in brackets:
+            top_ele=stack.pop() if stack else '#'
+            if brackets[char]!=top_ele:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+s="{[]}"
+print(isValid(s))
